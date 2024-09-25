232e9506bfd10afa8c33bc0618e773f4987b8a14
Daniel Hoffmann <hoffmann.daniel.1993@gmail.com>
2024-09-25 11:35:09 +0200

    Version 2.0.0

--- macros/migrate_materialisation.sql
+++ macros/migrate_materialisation.sql
@@ -1,48 +1,114 @@
-{% materialization migrate, adapter='trino' %}
+{% materialization incremental_migration, default -%}
 
-    {%- set existing_relation = load_cached_relation(this) -%}
-    {% set model_name = this.name %}
-    {% set target_relation = this.incorporate(type='table') %}
-    {%- set intermediate_relation = make_intermediate_relation(target_relation) -%}
-    {% set query_text %}
-        select * from {{ ref('migration_history') }} where model_name='{{ model_name }}'
-    {% endset %}
-    {% set existing_entry = run_query(query_text) %}
+  -- relations
+  {%- set existing_relation = load_cached_relation(this) -%}
+  {%- set target_relation = this.incorporate(type='table') -%}
+  {%- set temp_relation = make_temp_relation(target_relation)-%}
+  {%- set intermediate_relation = make_intermediate_relation(target_relation)-%}
+  {%- set backup_relation_type = 'table' if existing_relation is none else existing_relation.type -%}
+  {%- set backup_relation = make_backup_relation(target_relation, backup_relation_type) -%}
+  {%- set target_relation_name = this.name -%}
+  {% set query_text %}
+    select * from {{ ref('migration_history') }} where model_name='{{ target_relation_name }}'
+  {% endset %}
+  {%- set existing_entry = run_query(query_text) -%}
 
-    {% if existing_entry|length == 0 %}
-        {{ log('Running migration ' ~ model_name, info=True) }}
-        -- 'BEGIN' happens here
-        {{ run_hooks(pre_hooks, inside_transaction=True) }}
-        {% call statement('main') -%}
-            {{ get_create_table_as_sql(False, intermediate_relation, sql) }}
-        {%- endcall %}
-        {% call statement('main') -%}
-            insert into {{ ref('migration_history') }} (model_name,ran_at) values ('{{ model_name }}', {{ dbt.current_timestamp() }})
-        {%- endcall %}
+  -- configs
+  {%- set unique_key = config.get('unique_key') -%}
+  {%- set full_refresh_mode = (should_full_refresh()  or existing_relation.is_view) -%}
+  {%- set on_schema_change = incremental_validate_on_schema_change(config.get('on_schema_change'), default='ignore') -%}
 
-        -- cleanup
-        {% if existing_relation is not none %}
-            /* Do the equivalent of rename_if_exists. 'existing_relation' could have been dropped
-               since the variable was first set. */
-            {% set existing_relation = load_cached_relation(existing_relation) %}
-            {% if existing_relation is not none %}
-                {{ adapter.rename_relation(existing_relation, backup_relation) }}
-            {% endif %}
+  {% if existing_entry|length == 0 or full_refresh_mode %}
+
+      -- the temp_ and backup_ relations should not already exist in the database; get_relation
+      -- will return None in that case. Otherwise, we get a relation that we can drop
+      -- later, before we try to use this name for the current operation. This has to happen before
+      -- BEGIN, in a separate transaction
+      {%- set preexisting_intermediate_relation = load_cached_relation(intermediate_relation)-%}
+      {%- set preexisting_backup_relation = load_cached_relation(backup_relation) -%}
+       -- grab current tables grants config for comparision later on
+      {% set grant_config = config.get('grants') %}
+      {{ drop_relation_if_exists(preexisting_intermediate_relation) }}
+      {{ drop_relation_if_exists(preexisting_backup_relation) }}
+
+      {{ run_hooks(pre_hooks, inside_transaction=False) }}
+
+      -- `BEGIN` happens here:
+      {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
+      {% set to_drop = [] %}
+
+      {% if existing_relation is none %}
+          {% set build_sql = get_create_table_as_sql(False, target_relation, sql) %}
+      {% elif full_refresh_mode %}
+          {% set build_sql = get_create_table_as_sql(False, intermediate_relation, sql) %}
+          {% set need_swap = true %}
+      {% else %}
+        {% do run_query(get_create_table_as_sql(True, temp_relation, sql)) %}
+        {% do adapter.expand_target_column_types(
+                 from_relation=temp_relation,
+                 to_relation=target_relation) %}
+        {#-- Process schema changes. Returns dict of changes if successful. Use source columns for upserting/merging --#}
+        {% set dest_columns = process_schema_changes(on_schema_change, temp_relation, existing_relation) %}
+        {% if not dest_columns %}
+          {% set dest_columns = adapter.get_columns_in_relation(existing_relation) %}
         {% endif %}
 
-        {{ adapter.rename_relation(intermediate_relation, target_relation) }}
-        {% do create_indexes(target_relation) %}
+        {#-- Get the incremental_strategy, the macro to use for the strategy, and build the sql --#}
+        {% set incremental_strategy = config.get('incremental_strategy') or 'default' %}
+        {% set incremental_predicates = config.get('predicates', none) or config.get('incremental_predicates', none) %}
+        {% set strategy_sql_macro_func = adapter.get_incremental_strategy_macro(context, incremental_strategy) %}
+        {% set strategy_arg_dict = ({'target_relation': target_relation, 'temp_relation': temp_relation, 'unique_key': unique_key, 'dest_columns': dest_columns, 'incremental_predicates': incremental_predicates }) %}
+        {% set build_sql = strategy_sql_macro_func(strategy_arg_dict) %}
 
-        {{ run_hooks(post_hooks, inside_transaction=True) }}
-        -- 'COMMIT' happens here
-        {{ adapter.commit() }}
-        {{ return({'relations': [target_relation]}) }}
-    {% else %}
-        {{ log('Not running migration ' ~ model_name, info=True)}}
+      {% endif %}
+
+      {% call statement("main") %}
+          {{ build_sql }}
+      {% endcall %}
+
+      {% if full_refresh_mode %}
         {% call statement('main') -%}
-            {{ sql }}
+            delete from {{ ref('migration_history') }} where model_name='{{ target_relation_name }}'
         {%- endcall %}
-        {{ return({'relations': []}) }}
-    {% endif %}
+      {% endif %}
+      {% call statement('main') -%}
+         insert into {{ ref('migration_history') }} (model_name,ran_at) values ('{{ target_relation_name }}', {{ dbt.current_timestamp() }})
+      {%- endcall %}
+
+      {% if need_swap %}
+          {% do adapter.rename_relation(target_relation, backup_relation) %}
+          {% do adapter.rename_relation(intermediate_relation, target_relation) %}
+          {% do to_drop.append(backup_relation) %}
+      {% endif %}
+
+      {% set should_revoke = should_revoke(existing_relation, full_refresh_mode) %}
+      {% do apply_grants(target_relation, grant_config, should_revoke=should_revoke) %}
+
+      {% do persist_docs(target_relation, model) %}
+
+      {% if existing_relation is none or existing_relation.is_view or should_full_refresh() %}
+        {% do create_indexes(target_relation) %}
+      {% endif %}
+
+      {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+      -- `COMMIT` happens here
+      {% do adapter.commit() %}
+
+      {% for rel in to_drop %}
+          {% do adapter.drop_relation(rel) %}
+      {% endfor %}
+      
+      {{ run_hooks(post_hooks, inside_transaction=False) }}
+
+      {{ return({'relations': [target_relation]}) }}
+  {% else %}
+    {{ log('Not running migration ' ~ target_relation_name, info=True)}}
+    {% call statement('main') -%}
+        {{ sql }}
+    {%- endcall %}
+    {{ return({'relations': []}) }}
+  {% endif %}
 
 {% endmaterialization %}
